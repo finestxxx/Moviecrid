2a1d6724026354f39cab567386ad406d11d07caa
finestxxx <121338991+finestxxx@users.noreply.github.com>
2023-07-16 08:18:27 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const hamburger=document.querySelector(".hamburger");
+var menu=document.querySelector(".menu-list");
+
+hamburger.addEventListener("click", function(){
+    menu.classList.toggle('show');
+})
+
+
+
---
+++ singlepage.js
@@ -0,0 +1,8 @@
+let hamburgerSingle=document.querySelector(".hamburger-single");
+var menuSingle=document.querySelector(".menu-container-single");
+
+hamburgerSingle.addEventListener("click", function(){
+    menuSingle.classList.toggle('show-single');
+})
+
+

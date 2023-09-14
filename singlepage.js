7bd0c4f1c9db5903e5f7ec78d7cbe2ada935de2a
finestxxx <121338991+finestxxx@users.noreply.github.com>
2023-09-14 18:48:42 +0100

    Update singlepage.js

--- singlepage.js
+++ singlepage.js
@@ -6,22 +6,24 @@ hamburgerSingle.addEventListener("click", function(){
 })
 
 
-var links = document.getElementsByTagName("a");
-
-
-for (var i = 0; i < links.length; i++) {
-  // Get the current link's href attribute
-  var href = links[i].href;
-
-  
-  if (href.includes("gotaku1.com")) {
-   
-    var newHref = href.replace("gotaku1.com", "goone.pro");
-
-    
-    links[i].href = newHref;
+// Define a function that takes the old and new domain names as parameters
+function changeDomain(oldDomain, newDomain) {
+  // Select all the anchor tags on the page
+  var links = document.getElementsByTagName("a");
+  // Loop through each link
+  for (var i = 0; i < links.length; i++) {
+    // Get the href attribute of the link
+    var href = links[i].getAttribute("href");
+    // Check if the href contains the old domain name
+    if (href.indexOf(oldDomain) !== -1) {
+      // Replace the old domain name with the new one
+      var newHref = href.replace(oldDomain, newDomain);
+      // Set the new href attribute to the link
+      links[i].setAttribute("href", newHref);
+    }
   }
 }
 
-
-
+// Call the function with your desired domain names
+changeDomain("gotaku1.com", "go.com");
+changeDomain("goone.com", "goone.com");

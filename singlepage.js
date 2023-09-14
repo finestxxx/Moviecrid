a39b128970856923c035d73df3c5f679e6339473
finestxxx <121338991+finestxxx@users.noreply.github.com>
2023-09-14 17:56:26 +0100

    Update singlepage.js

--- singlepage.js
+++ singlepage.js
@@ -1,4 +1,20 @@
-let hamburgerSingle=document.querySelector(".hamburger-single");
+var links = document.getElementsByTagName("a");
+
+
+for (var i = 0; i < links.length; i++) {
+  // Get the current link's href attribute
+  var href = links[i].href;
+
+  
+  if (href.includes("gotaku1.com")) {
+   
+    var newHref = href.replace("gotaku1.com", "goone.pro");
+
+    
+    links[i].href = newHref;
+  }
+}
+ hamburgerSingle=document.querySelector(".hamburger-single");
 var menuSingle=document.querySelector(".menu-container-single");
 
 hamburgerSingle.addEventListener("click", function(){

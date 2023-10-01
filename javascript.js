2ee5807a2be50e2fde2d921903049d7f45657418
finestxxx <121338991+finestxxx@users.noreply.github.com>
2023-10-01 12:06:19 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,6 +9,6 @@ hamburger.addEventListener("click", function(){
 
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = '<script type="text/javascript" src="https://www.freefind.com/live/scripts/freefind_searchbox_v2.js"></script>
+script.src = 'https://www.freefind.com/live/scripts/freefind_searchbox_v2.js
 ';
 document.getElementsByTagName('head')[0].appendChild(script);
